8549fef7bb064d9e2f15c5b092116bc4ba83ea0a
TwanPaemeleire <161451506+TwanPaemeleire@users.noreply.github.com>
2025-08-06 23:46:51 +0200

    Made a new attack that shoots rows of projectiles towards the player transform.

---
+++ Assets/Scripts/Attacks/ProjectileRowToPlayerAttack.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ProjectileRowToPlayerAttack : BossAttack
+{
+    [SerializeField] private GameObject _projectileToShoot;
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private int _projectilesPerRow = 4;
+    [SerializeField] private float _delayBetweenProjectilesInRow = 0.05f;
+    [SerializeField] private int _amountOfRowsToShoot = 5;
+    [SerializeField] private float _delayBetweenRows = 0.5f;
+
+    public override void StartAttack()
+    {
+        StartCoroutine(ShootProjectileRows());
+    }
+
+    public override void StopAttackEarly()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator ShootProjectileRows()
+    {
+        for(int rowIdx = 0; rowIdx < _amountOfRowsToShoot; ++rowIdx)
+        {
+            Vector3 playerPosition = _playerTransform.position;
+            for(int projectileIdx = 0; projectileIdx < _projectilesPerRow; ++projectileIdx)
+            {
+                var projectileObj = ProjectilePool.Instance.GetProjectile(_projectileToShoot);
+                TestProjectile projectile = projectileObj.GetComponent<TestProjectile>();
+                projectile.transform.position = transform.position;
+                projectile.transform.right = (playerPosition - transform.position).normalized;
+                projectile.speedMultiplier = _projectileSpeedMultiplier;
+                if (projectileIdx == _projectilesPerRow -1) break;
+                yield return new WaitForSeconds(_delayBetweenProjectilesInRow);
+            }
+            if(rowIdx == _amountOfRowsToShoot - 1) break;
+            yield return new WaitForSeconds(_delayBetweenRows);
+        }
+
+        OnAttackFinished.Invoke();
+    }
+}
