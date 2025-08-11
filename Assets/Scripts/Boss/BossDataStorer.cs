eb05e71658e49f490231306fb05cb897a88840bf
TwanPaemeleire <161451506+TwanPaemeleire@users.noreply.github.com>
2025-08-11 11:01:11 +0200

    Made it so bosses get sorted per level of their versions.

--- Assets/Scripts/Boss/BossDataStorer.cs
+++ Assets/Scripts/Boss/BossDataStorer.cs
@@ -2,10 +2,12 @@ using System.Collections.Generic;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine;
+using JetBrains.Annotations;
 
 public class BossDataStorer : MonoBehaviour
 {
     private List<BossData> _bosses;
+    private List<List<int>> _bossesPerLevel = new List<List<int>>();
 
     private void Start()
     {
@@ -19,11 +21,7 @@ public class BossDataStorer : MonoBehaviour
         if (handle.Status == AsyncOperationStatus.Succeeded)
         {
             _bosses = new List<BossData>(handle.Result);
-            for (int i = 0; i < 20; ++i)
-            {
-                BossData boss = GetRandomBoss();
-                Debug.Log(boss.Name + " " + boss.ID);
-            }
+            SortBossesPerLevel();
         }
         else
         {
@@ -31,9 +29,25 @@ public class BossDataStorer : MonoBehaviour
         }
     }
 
-    public BossData GetRandomBoss()
+    private void SortBossesPerLevel()
     {
-        int randomBoss = Random.Range(0, _bosses.Count);
-        return _bosses[randomBoss];
+        for(int bossIdx = 0; bossIdx < _bosses.Count; ++bossIdx)
+        {
+            foreach(BossVersionData bossVersion in _bosses[bossIdx].Versions)
+            {
+                int bossLevel = bossVersion.Level;
+                while(_bossesPerLevel.Count < bossLevel)
+                {
+                    _bossesPerLevel.Add(new List<int>());
+                }
+                _bossesPerLevel[bossLevel - 1].Add(bossIdx);
+            }
+        }
+    }
+
+    public BossData GetRandomBoss(int level)
+    {
+        int randomBoss = Random.Range(0, _bossesPerLevel[level].Count);
+        return _bosses[_bossesPerLevel[level][randomBoss]];
     }
 }
